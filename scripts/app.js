562b7ac3e2e5e34c41e3a14dd8c3ee42887574e9
jane muthoni <muchirijane@outlook.com>
2020-06-01 08:49:22 +0300

    updating the ui js file

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,9 @@
-//class instance
+// dom queries
 const chatList = document.querySelector('.chat-list');
 
+// class instances
 const chatUI = new ChatUI(chatList);
-const chatroom = new Chatroom("general", "rio");
+const chatroom = new Chatroom('gaming', 'shaun');
 
-//gets chat render
-chatroom.getChats(data=> chatUI.render(data));
+// get chats & render
+chatroom.getChats(data => chatUI.render(data));
