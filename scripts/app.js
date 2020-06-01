357c6eefc05fdb955a3e0087eeb4b0bd36f19794
jane muthoni <muchirijane@outlook.com>
2020-06-01 15:33:37 +0300

    added event listener for updatinf username

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,33 @@
 // dom queries
 const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+const newNameForm = document.querySelector('.new-name');
+const updateMssg = document.querySelector('.update-mssg');
+
+//add a new chat
+newChatForm.addEventListener('submit', e =>{
+    e.preventDefault();
+    const message = newChatForm.message.value.trim();
+    chatroom.addChat(message)
+      .then(() => newChatForm.reset())
+      .catch(err => console.log(err));
+});
+
+//update username
+newNameForm.addEventListener('submit', e =>{
+    e.preventDefault();
+
+    const newName = newNameForm.name.value.trim();
+
+    chatroom.updateUsername(newName);
+
+    updateMssg.innerText =  `You updated your name to: ${newName}`;
+
+    newNameForm.reset();
+
+    setTimeout(()=>updateMssg.innerText = '', 3000);
+})
+
 
 // class instances
 const chatUI = new ChatUI(chatList);
